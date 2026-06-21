ec1a3b5b7f327ff9dcb906cfb5825c29ad89089e
riiqzin <futeuropeuu@gmail.com>
2026-06-21 12:07:00 -0300

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -1,7 +1,7 @@
 const STORE_ID = process.env.NUVEMSHOP_STORE_ID || '5191961';
 const TOKEN = process.env.NUVEMSHOP_TOKEN || '05fce3e924caa8a1447f6387fda922c31e8f00a0';
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();
